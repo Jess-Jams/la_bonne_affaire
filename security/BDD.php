bc0a11d8e5f3bca7e91f101a8b988a149d04cbe4
Jess-Jams <jchandler971@gmail.com>
2023-05-08 11:41:26 -0400

    skeleton web

---
+++ security/BDD.php
@@ -0,0 +1,10 @@
+<?php 
+define("USER", "root");
+define("PASSWORD", "");
+define("DSN", "mysql:host=localhost;dbname=labonneaffaire");
+
+try {
+    $pdo = new PDO(DSN, USER, PASSWORD);
+} catch (PDOException $e) {
+    die("Error ! : ".$e->getMessage());
+}
